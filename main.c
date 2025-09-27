aa993306c67f6fddb1d2e0991e14aa2e52f219a2
michelle3555 <michelle3555@snu.ac.kr>
2025-09-27 17:16:51 +0900

    ?

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main(){
 
-    printf("hello, world-by jiseu\n");
+    printf("hello, world?-by jiseu\n")
     printf("hello, world - by j1\n");
     printf("hello world! - by michelle3555\n");
 
